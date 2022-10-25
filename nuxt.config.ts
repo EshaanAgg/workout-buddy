c066f904e0d21b67027be79d87293be5f98b653a
Eshaan Aggarwal <eshaanagg@gmail.com>
2022-10-25 06:21:18 +0530

    Disable SSR and fallback to SPA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   css: ["@/assets/css/styles.css"],
   buildModules: ["@pinia/nuxt"],
   runtimeConfig: {
